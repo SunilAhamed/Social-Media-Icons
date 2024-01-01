f32706af6a740560af9482764fba47d299ddb09d
SUNIL AHAMED I <144587094+SunilAhamed@users.noreply.github.com>
2024-01-01 18:59:29 +0530

    Add files via upload

---
+++ Social Media Icons/script.js
@@ -0,0 +1,19 @@
+document.getElementById("facebook").addEventListener("click",function() {
+    window.open("https://www.facebook.com/","_blank");
+});
+
+document.getElementById("twitter").addEventListener("click",function() {
+    window.open("https://twitter.com/","_blank");
+});
+
+document.getElementById("instagram").addEventListener("click",function() {
+    window.open("https://www.instagram.com/","_blank");
+});
+
+document.getElementById("linkedin").addEventListener("click",function() {
+    window.open("https://www.linkedin.com/","_blank");
+});
+
+document.getElementById("whatsapp").addEventListener("click",function() {
+    window.open("https://wa.me/yourwhatsappphonenumber","_blank");
+});
